62aa95ea85c236e0bb2d04115c9a2e322ec792e2
Marie Hughey <hughey0@purdue.edu>
2018-06-19 15:40:58 -0400

    Add files via upload
    Saves as json file now

--- visionApiCall.js
+++ visionApiCall.js
@@ -8,14 +8,20 @@ const client = new vision.ImageAnnotatorClient();
 const fileName = ericInput;
 
 // Read a local image as a text document
-// Saves in variable result 
-var result = "";
+// Saves response in json file
+var json_result = {
+    response:[]
+};
 client
   .documentTextDetection(fileName)
   .then(results => {
       const fullTextAnnotation = results[0].fullTextAnnotation;
-      result = fullTextAnnotation.text;
-      //console.log(result);
+      const text = fullTextAnnotation.text;
+      json_result.response.push({fullTextAnnotation});
+      json_result.response.push({text});
+      var json = JSON.stringify(json_result, null, "\t");
+      var fs = require('fs');
+      fs.writeFile('image_text.json', json, 'utf8');
   })
   .catch(err => {
       console.error('ERROR:', err);
