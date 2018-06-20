4d5dac7c32fc4fe884d35d00e40b2052ddf82772
erickimme <eric.kim@genesys.com>
2018-06-20 12:32:29 -0400

    screen-capture gets full page but not specific reactangle area

---
+++ screen_capture/background.js
@@ -0,0 +1,75 @@
+// 'use strict';
+  // Called when the user clicks on the browser action.
+//   chrome.browserAction.onClicked.addListener(function (tab) {
+//     var s = document.createElement('script');
+//     s.src = chrome.extension.getURL('api_ezcopy.js');
+//     s.onload = function() {
+//         // draw rectangle
+//         alert("here");
+//         var c = document.body;
+//         var ctx=c.getContext("2d");
+//         ctx.rect(20,20,150,100);
+//         ctx.stroke();
+
+//         this.remove();
+//     };
+//     (document.head || document.documentElement).appendChild(s);
+
+
+// chrome.tabs.executeScript({
+    //   code: 'alert(this.initDraw(document.body))' 
+      
+    // });
+
+    // // let user draw the rectangle to obtain coordinates of the image.
+    // function initDraw(canvas) {
+    //     alert("We inside of initDraw");
+    //     // alert(canvas);
+    //     var mouse = {
+    //         x: 0,
+    //         y: 0,
+    //         startX: 0,
+    //         startY: 0
+    //     };
+    //     function setMousePosition(e) {
+    //         var ev = e || window.event; //Moz || IE
+    //         if (ev.pageX) { //Moz
+    //             mouse.x = ev.pageX + window.pageXOffset;
+    //             mouse.y = ev.pageY + window.pageYOffset;
+    //         } else if (ev.clientX) { //IE
+    //             mouse.x = ev.clientX + document.body.scrollLeft;
+    //             mouse.y = ev.clientY + document.body.scrollTop;
+    //         }
+    //     };
+    
+    //     var element = null;    
+    //     canvas.onmousemove = function (e) {
+    //         setMousePosition(e);
+    //         if (element !== null) {
+    //             element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
+    //             element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+    //             element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
+    //             element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+    //         }
+    //     }
+    //     // alert("let's click it");
+    //     canvas.onclick = function (e) {
+    //         if (element !== null) {
+    //             element = null;
+    //             canvas.style.cursor = "default";
+    //             console.log("finsihed.");
+    //         } else {
+    //             console.log("begun.");
+    //             mouse.startX = mouse.x;
+    //             mouse.startY = mouse.y;
+    //             element = document.createElement('div');
+    //             element.className = 'rectangle'
+    //             element.style.left = mouse.x + 'px';
+    //             element.style.top = mouse.y + 'px';
+    //             canvas.appendChild(element)
+    //             canvas.style.cursor = "crosshair";
+    //         }
+    //     }
+    // }
+
+//   });
